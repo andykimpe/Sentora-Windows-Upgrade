8ec4ad6fe1670c1235ce4bb8881c716c7d56246a
Kimpe Andy <andykimpe@gmail.com>
2022-12-22 12:33:18 +0100

    Update controller.ext.php

--- 1.0.3/panel/modules/cron/code/controller.ext.php
+++ 1.0.3/panel/modules/cron/code/controller.ext.php
@@ -236,6 +236,9 @@ class module_controller extends ctrl_module
         if ($numrows->fetchColumn() <> 0) {
             $sql = $zdbh->prepare($sql);
             $sql->execute();
+            if (sys_versions::ShowOSPlatformVersion() == "Windows") {
+            	$line .= "" . ctrl_options::GetSystemOption('daemon_timing') . " " . ctrl_options::GetSystemOption('php_exer') . " " . ctrl_options::GetSystemOption('daemon_exer') . "" . fs_filehandler::NewLine();
+            } else { 
             $line .= "#################################################################################" . fs_filehandler::NewLine();
             $line .= "# CRONTAB FOR ZPANEL CRON MANAGER MODULE                                         " . fs_filehandler::NewLine();
             $line .= "# Module Developed by Bobby Allen, 17/12/2009                                    " . fs_filehandler::NewLine();
@@ -243,29 +246,21 @@ class module_controller extends ctrl_module
             $line .= "#################################################################################" . fs_filehandler::NewLine();
             $line .= "# WE DO NOT RECOMMEND YOU MODIFY THIS FILE DIRECTLY, PLEASE USE ZPANEL INSTEAD!  " . fs_filehandler::NewLine();
             $line .= "#################################################################################" . fs_filehandler::NewLine();
-            if (sys_versions::ShowOSPlatformVersion() == "Windows") {
-                $line .= "# Cron Debug infomation can be found in this file here:-                        " . fs_filehandler::NewLine();
-                $line .= "# C:\WINDOWS\System32\crontab.txt                                                " . fs_filehandler::NewLine();
-                $line .= "#################################################################################" . fs_filehandler::NewLine();
-// removed openbase_dir and suhosin directives from daemon run - daemon now runs just fine on windows
-                //$line .= "" . ctrl_options::GetSystemOption('daemon_timing') . " " . ctrl_options::GetSystemOption('php_exer') . " -d suhosin.executor.func.blacklist=\"passthru, show_source, shell_exec, system, pcntl_exec, popen, pclose, proc_open, proc_nice, proc_terminate, proc_get_status, proc_close, leak, apache_child_terminate, posix_kill, posix_mkfifo, posix_setpgid, posix_setsid, posix_setuid, escapeshellcmd, escapeshellarg, exec\" -d open_basedir=\"" . ctrl_options::GetSystemOption('hosted_dir') . $currentuser['username'] . "/" . ctrl_options::GetSystemOption('openbase_seperator') . ctrl_options::GetSystemOption('openbase_temp') . "\" " . ctrl_options::GetSystemOption('daemon_exer') . "" . fs_filehandler::NewLine();
-				$line .= "" . ctrl_options::GetSystemOption('daemon_timing') . " " . ctrl_options::GetSystemOption('php_exer') . " " . ctrl_options::GetSystemOption('daemon_exer') . "" . fs_filehandler::NewLine();
-                $line .= "#################################################################################" . fs_filehandler::NewLine();
-            }
             $line .= "# DO NOT MANUALLY REMOVE ANY OF THE CRON ENTRIES FROM THIS FILE, USE ZPANEL      " . fs_filehandler::NewLine();
             $line .= "# INSTEAD! THE ABOVE ENTRIES ARE USED FOR ZPANEL TASKS, DO NOT REMOVE THEM!      " . fs_filehandler::NewLine();
             $line .= "#################################################################################" . fs_filehandler::NewLine();
+	    }
             while ($rowcron = $sql->fetch()) {
                 $fetchRows = $zdbh->prepare("SELECT * FROM x_accounts WHERE ac_id_pk=:userid AND ac_deleted_ts IS NULL");
                 $fetchRows->bindParam(':userid', $rowcron['ct_acc_fk']);
                 $fetchRows->execute();
                 $rowclient = $fetchRows->fetch();
                 if ($rowclient && $rowclient['ac_enabled_in'] <> 0) {
-                    $line .= "# CRON ID: " . $rowcron['ct_id_pk'] . "" . fs_filehandler::NewLine();
+                    //$line .= "# CRON ID: " . $rowcron['ct_id_pk'] . "" . fs_filehandler::NewLine();
 // removed openbase_dir and suhosin directives - allows proper running of jobs on Windows
                     //$line .= "" . $rowcron['ct_timing_vc'] . " " . ctrl_options::GetSystemOption('php_exer') . " -d suhosin.executor.func.blacklist=\"passthru, show_source, shell_exec, system, pcntl_exec, popen, pclose, proc_open, proc_nice, proc_terminate, proc_get_status, proc_close, leak, apache_child_terminate, posix_kill, posix_mkfifo, posix_setpgid, posix_setsid, posix_setuid, escapeshellcmd, escapeshellarg, exec\" -d open_basedir=\"" . ctrl_options::GetSystemOption('hosted_dir') . $currentuser['username'] . "/" . ctrl_options::GetSystemOption('openbase_seperator') . ctrl_options::GetSystemOption('openbase_temp') . "\" " . $rowcron['ct_fullpath_vc'] . "" . fs_filehandler::NewLine();
 					$line .= "" . $rowcron['ct_timing_vc'] . " " . ctrl_options::GetSystemOption('php_exer') . " " . $rowcron['ct_fullpath_vc'] . "" . fs_filehandler::NewLine();
-                    $line .= "# END CRON ID: " . $rowcron['ct_id_pk'] . "" . fs_filehandler::NewLine();
+                    //$line .= "# END CRON ID: " . $rowcron['ct_id_pk'] . "" . fs_filehandler::NewLine();
                 }
             }
 
@@ -286,6 +281,9 @@ class module_controller extends ctrl_module
                 return false;
             }
         } else {
+            if (sys_versions::ShowOSPlatformVersion() == "Windows") {
+            	$line .= "" . ctrl_options::GetSystemOption('daemon_timing') . " " . ctrl_options::GetSystemOption('php_exer') . " " . ctrl_options::GetSystemOption('daemon_exer') . "" . fs_filehandler::NewLine();
+            } else {		
             $line .= "#################################################################################" . fs_filehandler::NewLine();
             $line .= "# CRONTAB FOR ZPANEL CRON MANAGER MODULE                                         " . fs_filehandler::NewLine();
             $line .= "# Module Developed by Bobby Allen, 17/12/2009                                    " . fs_filehandler::NewLine();
@@ -293,16 +291,10 @@ class module_controller extends ctrl_module
             $line .= "#################################################################################" . fs_filehandler::NewLine();
             $line .= "# WE DO NOT RECOMMEND YOU MODIFY THIS FILE DIRECTLY, PLEASE USE ZPANEL INSTEAD!  " . fs_filehandler::NewLine();
             $line .= "#################################################################################" . fs_filehandler::NewLine();
-            if (sys_versions::ShowOSPlatformVersion() == "Windows") {
-                $line .= "# Cron Debug infomation can be found in this file here:-                        " . fs_filehandler::NewLine();
-                $line .= "# C:\WINDOWS\System32\crontab.txt                                                " . fs_filehandler::NewLine();
-                $line .= "#################################################################################" . fs_filehandler::NewLine();
-                $line .= "" . ctrl_options::GetSystemOption('daemon_timing') . " " . ctrl_options::GetSystemOption('php_exer') . " " . ctrl_options::GetSystemOption('daemon_exer') . "" . fs_filehandler::NewLine();
-                $line .= "#################################################################################" . fs_filehandler::NewLine();
-            }
             $line .= "# DO NOT MANUALLY REMOVE ANY OF THE CRON ENTRIES FROM THIS FILE, USE ZPANEL      " . fs_filehandler::NewLine();
             $line .= "# INSTEAD! THE ABOVE ENTRIES ARE USED FOR ZPANEL TASKS, DO NOT REMOVE THEM!      " . fs_filehandler::NewLine();
             $line .= "#################################################################################" . fs_filehandler::NewLine();
+	    }
             if (fs_filehandler::UpdateFile(ctrl_options::GetSystemOption('cron_file'), 0644, $line)) {
                 if (sys_versions::ShowOSPlatformVersion() != "Windows") {
                     $returnValue = ctrl_system::systemCommand(
